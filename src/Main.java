bc94883fb1a72e597f70911e252bc2403b61be82
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2025-08-30 11:37:49 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -37,9 +37,6 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.log.makeFile();
-        main.log.append("#######################################################");
-        main.log.append("####################PROGRAM-STARTED####################");
-        main.log.append("#######################################################");
 
         main.log.append("MADE BY FEDI6431");
 
